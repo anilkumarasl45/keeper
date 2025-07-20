ebdafca49d6d0e4567a0bc8a64207b8297251ac5
anilkumarasl45 <104633589+anilkumarasl45@users.noreply.github.com>
2025-07-20 08:06:08 +0530

    Updated Note.jsx

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -205,7 +205,6 @@ function NoteArea() {
           <motion.div
             className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
             variants={containerVariants}
-            initial="hidden"
             animate="visible"
           >
             <AnimatePresence>
